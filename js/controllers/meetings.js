af106726329b761e1fc17090192f113f888d8898
victor-basurto <my.mvplace@gmail.com>
2016-06-18 13:41:13 -0700

    user can add new meetings

--- js/controllers/meetings.js
+++ js/controllers/meetings.js
@@ -1,4 +1,36 @@
 'use strict';
-myApp.controller( 'MeetingsController', [ '$scope', function( $scope ) {
-	
+myApp.controller( 'MeetingsController', [ 
+	'$scope', 
+	'$rootScope',
+	'$firebaseAuth',
+	'$firebaseArray',
+	'FIREBASE_URL',
+	function( $scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL ) {
+		
+		var ref = new Firebase( FIREBASE_URL ),
+			auth = $firebaseAuth( ref );
+
+		// if user exists
+		auth.$onAuth(function( authUser ) {
+			if ( authUser ) {
+				var meetingsRef, meetingsInfo;
+
+				meetingsRef = 
+					new Firebase( FIREBASE_URL + 'user/' + $rootScope.currentUser.$id + '/meetings' );			
+				meetingsInfo = $firebaseArray( meetingsRef );
+
+				// list of meetings into the meetings model
+				$scope.meetings = meetingsInfo;
+
+				// add new meeting
+				$scope.addMeeting = function() {
+					meetingsInfo.$add({
+						name: $scope.meetingname,
+						date: Firebase.ServerValue.TIMESTAMP
+					}).then( function() {
+						$scope.meetingname = '';
+					});
+				}
+			}
+		});
 }]);
